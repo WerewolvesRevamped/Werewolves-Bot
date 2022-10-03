70e3126cbd3a5d6c3024c287ea9bbb8a13f51657
McTsts <lpmctsts@gmail.com>
2022-10-03 12:51:05 +0000

    rerun role removal

--- game.js
+++ game.js
@@ -388,7 +388,9 @@ module.exports = function() {
 		removeNicknameOnce(channel, channel.guild.roles.cache.get(stats.participant).members.toJSON(), 0);
 		removeNicknameOnce(channel, channel.guild.roles.cache.get(stats.dead_participant).members.toJSON(), 0);
 		// Remove Roles & Nicknames
-		wroles_remove(channel, [stats.signed_up, stats.participant, stats.dead_participant, stats.spectator, stats.mayor, stats.mayor2, stats.reporter, stats.guardian, stats.sub], ["signed up", "participant", "dead participant", "spectator", "mayor", "mayor2", "reporter", "guardian", "substitute"])
+		wroles_remove(channel, [stats.signed_up, stats.spectator, stats.mayor, stats.mayor2, stats.reporter, stats.guardian, stats.sub, stats.participant, stats.dead_participant], ["signed up", "spectator", "mayor", "mayor2", "reporter", "guardian", "substitute", "participant", "dead participant"]);
+        // run role removal again for critical roles because sometimes it fails even though it says it succeeds
+		wroles_remove(channel, [stats.participant, stats.dead_participant], ["participant", "dead participant"]);
 		// Cleanup channels
 		cmdCCCleanup(channel);
 		cmdRolesScCleanup(channel);
