a4a270b14e5b0049a7657d573195a44b305276fa
McTsts <lpmctsts@gmail.com>
2025-06-25 11:24:21 +0200

    log5/6

--- utility/confirm.js
+++ utility/confirm.js
@@ -53,6 +53,8 @@ module.exports = function() {
 			case "players log2": cmdPlayersLog2(message.channel); break;
 			case "players log3": cmdPlayersLog3(message.channel); break;
 			case "players log4": cmdPlayersLog4(message.channel); break;
+			case "players log5": cmdPlayersLog(message.channel, "-"); break;
+			case "players log6": cmdPlayersLog3(message.channel, "-"); break;
 			case "players votes": cmdPlayersVotes(message.channel); break;
 			case "players roles": cmdPlayersRoleList(message.channel); break;
 			case "cc cleanup": cmdCCCleanup(message.channel); break;
